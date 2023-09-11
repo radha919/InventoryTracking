cb33d9c7d1d0dc07e04aea89cf0ac498fb4ba090
Raghav <70934347+raggzy271@users.noreply.github.com>
2023-09-11 16:18:32 +0530

    Added Customer, SalesOrder & SalesOrderLineItem entities.

---
+++ entity/SalesOrderLineItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace entity
+{
+    public class SalesOrderLineItem
+    {
+        [Key]
+        public int SalesOrderLineItemId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        // Other sales order line item properties
+
+        [ForeignKey("SalesOrder")]
+        public int SalesOrderId { get; set; }
+        public virtual SalesOrder SalesOrder { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
