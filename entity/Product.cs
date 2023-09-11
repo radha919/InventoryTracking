ec776ed6b5c7e8f523d627d631df8c25c75fc8bd
radhaagrawal919 <radhaagrawal919@gmail.com>
2023-09-11 05:14:52 +0000

    Add project files.

---
+++ entity/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace entity
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        // Other product properties
+
+        public virtual ICollection<PurchaseLineItem> PurchaseLineItems { get; set; }
+        public virtual ICollection<SalesOrderLineItem> SalesOrderLineItems { get; set; }
+    }
+
+    public class Supplier
+    {
+        [Key]
+        public int SupplierId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Other supplier properties
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+    }
+
+     
+}
+
+ 
+
